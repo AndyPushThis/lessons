6e2c59de4b1a52662f95845206cc005107de35e7
andty <andrymark00@gmail.com>
2024-04-04 17:57:23 +0300

    fix changes

--- routes/web-old.php
+++ routes/web-old.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 //Route::get('/', [PostController::class, 'index'])->name('posts.index');
@@ -13,4 +14,20 @@ use Illuminate\Support\Facades\Route;
 
 //Route::redirect('/', 'posts');
 Route::resource('posts', PostController::class);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
